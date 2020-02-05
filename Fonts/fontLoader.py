ee931ac1810afbefb4a9a0bb78e331842c9c773e
Jake <jake.gads@gmail.com>
2020-02-05 09:00:06 -0500

    added forced linux functionality

--- Fonts/fontLoader.py
+++ Fonts/fontLoader.py
@@ -1,14 +1,19 @@
 import os 
 from subprocess import call
 from glob import glob
+import platform
 
-try:
-    os.system("mkdir -p ~/.local/share/fonts")
-except:
-    print("Folder Already Generated")
+if platform.system() == "Linux":
+    try:
+        os.system("mkdir -p ~/.local/share/fonts")
+    except:
+        print("Folder Already Generated")
 
-all_files = glob(f'{os.getcwd()}/**/*', recursive=True)
-for f in all_files:
-    os.system(f"cp {f} ~/.local/share/fonts/")
+    all_files = glob(f'{os.getcwd()}/**/*', recursive=True)
 
-os.system("fc-cache -f -v")
+    for f in all_files:
+        os.system(f"cp {f} ~/.local/share/fonts/")
+
+    os.system("fc-cache -f -v")
+else:
+    print("Sorry This only works on linux machines :(")
